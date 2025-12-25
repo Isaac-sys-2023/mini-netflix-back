5eb4bc95a7961d90600228f49d704400ee122534
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-12-25 01:14:19 -0400

    feat: add Episodio entity

--- src/episodio/entities/episodio.entity.ts
+++ src/episodio/entities/episodio.entity.ts
@@ -1,1 +1,16 @@
-export class Episodio {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Episodio {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  titulo: string;
+
+  @Column()
+  duracion: number;
+
+  @Column()
+  numeroCapitulo: number;
+}
