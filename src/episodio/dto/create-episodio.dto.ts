8c8408d7a0ae226717fadcafd946ae0d1b74306e
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-12-25 02:32:21 -0400

    feat: Add the DTOs to episodios and series and configure the ValidationPipe

--- src/episodio/dto/create-episodio.dto.ts
+++ src/episodio/dto/create-episodio.dto.ts
@@ -1,1 +1,25 @@
-export class CreateEpisodioDto {}
+import { Type } from 'class-transformer';
+import { IsNumber, IsPositive, IsString } from 'class-validator';
+
+export class CreateEpisodioDto {
+  @IsString({ message: 'El título debe ser una cadena de texto' })
+  titulo: string;
+
+  @Type(() => Number)
+  //Esto porque la duracion en minutos no siempre es un entero exacto puede ser 45.5 minutos por ejemplo
+  @IsNumber({}, { message: 'La duración debe ser un número en minutos' })
+  @IsPositive({ message: 'La duración debe ser un número positivo' })
+  duracion: number;
+
+  @Type(() => Number)
+  //Esto porque pueden existir episodios intermedios que no tengan un numero entero como tal digamos unn episodio 8.5 que es un especial que va entre el 8 y el 9
+  @IsNumber({}, { message: 'El número de capítulo debe ser un número' })
+  @IsPositive({ message: 'El número de capítulo debe ser un número positivo' })
+  numeroCapitulo: number;
+
+  @IsString({
+    message:
+      'El nombre de la serie a la que pertenece el episodio debe ser una cadena de texto',
+  })
+  serie: string;
+}
--- src/serie/dto/create-serie.dto.ts
+++ src/serie/dto/create-serie.dto.ts
@@ -1,1 +1,19 @@
-export class CreateSerieDto {}
+import { IsString } from 'class-validator';
+
+export class CreateSerieDto {
+  @IsString({ message: 'El título debe ser una cadena de texto' })
+  titulo: string;
+
+  @IsString({ message: 'El genero debe ser una cadena de texto' })
+  genero: string;
+
+  @IsString({
+    message: 'La sinopsis debe ser una cadena de texto',
+  })
+  sinopsis: string;
+
+  @IsString({
+    message: 'La url de la portada debe ser una cadena de texto de un enlace',
+  })
+  urlPortada: string;
+}
