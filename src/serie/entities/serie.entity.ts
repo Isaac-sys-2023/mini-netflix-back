a2687176c6ed87db9517aefffc4f9aa38de509ce
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-12-25 01:11:51 -0400

    feat: add Serie entity

--- src/serie/entities/serie.entity.ts
+++ src/serie/entities/serie.entity.ts
@@ -1,1 +1,19 @@
-export class Serie {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Serie {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  titulo: string;
+
+  @Column()
+  genero: string;
+
+  @Column()
+  sinopsis: string;
+
+  @Column()
+  urlPortada: string;
+}
